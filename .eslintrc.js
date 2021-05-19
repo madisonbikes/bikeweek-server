f0d93464ee3bb44f9b4e1e358c9e2718b5d30cfb
tsandee <tbsandee@orangebikelabs.com>
2021-05-19 16:09:28 -0500

    initial import

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+    jest: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 6
+  },
+  plugins: ["@typescript-eslint"],
+};
