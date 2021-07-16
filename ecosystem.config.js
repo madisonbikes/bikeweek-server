f085cfbad178344e4761ce1692ab595bca3b91c5
tsandee <tbsandee@orangebikelabs.com>
2021-07-16 14:15:54 -0500

    update ecosystem for deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      script: "./index.js",
+      script: "./dist/index.js",
     },
   ],
 
@@ -9,12 +9,12 @@ module.exports = {
     production: {
       user: "madisonbikes",
       host: "mbi",
-      ref: "origin/master",
+      ref: "origin/main",
       repo: "https://github.com/madisonbikes/bikeweek2021-importer",
-      path: "/home/madisonbikes/apps/bikeweek2021_importer/repo",
+      path: "/home/madisonbikes/apps/bikeweek2021_importer",
       "pre-deploy-local": "",
       "post-deploy":
-        "npm install && pm2 reload ecosystem.config.js --env prod",
+        "npm install && pm2 reload ecosystem.config.js --env production",
       "pre-setup": "",
     },
   },
