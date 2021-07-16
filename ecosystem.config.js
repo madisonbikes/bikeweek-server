eada3de499af25806ec0b87d99dcec83de488a80
tsandee <tbsandee@orangebikelabs.com>
2021-07-16 15:51:42 -0500

    new ecosystem fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
       path: "/home/madisonbikes/apps/bikeweek2021_importer",
       "pre-deploy-local": "",
       "post-deploy":
-        "npm install && npm run pm2-start",
+        "npm install && npm prune && npm run pm2-start",
       "pre-setup": "",
     },
   },
