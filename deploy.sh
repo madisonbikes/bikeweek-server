a9cae8dacd3597970860f8631a00d4826e17fb11
tsandee <tbsandee@orangebikelabs.com>
2021-07-21 14:24:48 -0500

    fix up deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,6 @@
-ssh -t mbi "cd ~/apps/bikeweek2021_importer/app; git pull; npm i; npm prune; pm2 reload ecosystem.config.js"
+ssh -t mbi "cd ~/apps/bikeweek2021_importer/app; \
+            git reset --hard origin/main; \
+            git pull; \
+            npm i; \
+            npm prune; \
+            pm2 reload ecosystem.config.js"
