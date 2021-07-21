2539d55a598c11c601b6bae54b5332a0bf52ccf8
tsandee <tbsandee@orangebikelabs.com>
2021-07-21 14:30:28 -0500

    deploy script update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,16 @@
-ssh -t mbi "cd ~/apps/bikeweek2021_importer/app; \
-            git reset --hard origin/main; \
+# simple deploy script, adjust accordingly
+HOSTNAME=mbi
+
+# cloned git repo path, will
+REPO_PATH="~/apps/bikeweek2021_importer/app"
+
+# git branch to use
+GIT_BRANCH=origin/main
+
+# resets git branch and pulls new code, install/removes packages as necessary and reloads the pm2 process
+ssh -t $HOSTNAME "\
+            cd $REPO_PATH; \
+            git reset --hard $GIT_BRANCH; \
             git pull; \
             npm i; \
             npm prune; \
