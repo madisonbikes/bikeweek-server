c5a9f0bf0a41f4a918bcc75a2c284f57a14621b6
tsandee <tbsandee@orangebikelabs.com>
2021-07-23 17:32:07 -0500

    fix ts-node location

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "bikeweek2021-importer",
-      script: "node-modules/.bin/ts-node",
+      script: "node_modules/.bin/ts-node",
       args: "src/index.ts",
       log_date_format: "YYYY-MM-DD HH:mm Z"
     },
