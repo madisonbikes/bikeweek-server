cd9aee1544ce53685aaa7d008099b5683ee2c3db
tsandee <tbsandee@orangebikelabs.com>
2021-07-23 14:12:05 -0500

    change pm2 name
    Signed-off-by: tsandee <tbsandee@orangebikelabs.com>

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   apps: [
     {
+      name: "bikeweek2021-importer",
       script: "node-modules/.bin/ts-node",
       args: "src/index.ts",
       log_date_format: "YYYY-MM-DD HH:mm Z"
