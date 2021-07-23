3073b8a7bc9aaac3acd631cc433475cab53ff47d
tsandee <tbsandee@orangebikelabs.com>
2021-07-23 14:09:16 -0500

    use tsnode/bin
    Signed-off-by: tsandee <tbsandee@orangebikelabs.com>

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      script: "ts-node",
+      script: "node-modules/.bin/ts-node",
       args: "src/index.ts",
       log_date_format: "YYYY-MM-DD HH:mm Z"
     },
